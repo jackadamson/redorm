f700a5015649e6a2aaed763a1eb6b4f162c01e58
Jack Adamson <jack@mrfluffybunny.com>
2021-01-04 18:18:45 +1100

    comment out

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from redorm import red
 
 @pytest.fixture
 def clean_db():
-    red.bind("redis://localhost")
+    # red.bind("redis://localhost")
     red.client.flushdb(asynchronous=False)
     red.setup_scripts()
